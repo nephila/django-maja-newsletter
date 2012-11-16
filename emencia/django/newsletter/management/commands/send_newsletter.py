1409e84b04f5edf08d81b2509b46b2a67ca682c4
Iacopo Spalletti <i.spalletti@nephila.it>
2012-11-16 19:23:03 +0100

    Fix a unicode error in management command

--- emencia/django/newsletter/management/commands/send_newsletter.py
+++ emencia/django/newsletter/management/commands/send_newsletter.py
@@ -5,7 +5,7 @@ from django.core.management.base import NoArgsCommand
 
 from emencia.django.newsletter.mailer import Mailer
 from emencia.django.newsletter.models import Newsletter
-
+import codecs
 
 class Command(NoArgsCommand):
     """Send the newsletter in queue"""
@@ -24,7 +24,7 @@ class Command(NoArgsCommand):
             mailer = Mailer(newsletter, verbose=verbose)
             if mailer.can_send:
                 if verbose:
-                    print 'Start emailing %s' % newsletter.title
+                    print 'Start emailing %s' % codecs.encode(newsletter.title,"utf-8")
                 mailer.run()
 
         if verbose:
