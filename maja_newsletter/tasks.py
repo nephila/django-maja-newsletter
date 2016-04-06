2435cec512d8aa8566d2a38215d932db46a21430
Iacopo Spalletti <i.spalletti@nephila.it>
2016-04-06 20:01:08 +0200

    Add offloading export to celery

--- maja_newsletter/tasks.py
+++ maja_newsletter/tasks.py
@@ -1,8 +1,19 @@
 from __future__ import absolute_import
+
+import shutil
+
+import codecs
+import os.path
+from tempfile import mkdtemp
+
 from celery import shared_task
+from django.conf import settings
+from django.core.mail import EmailMessage
 
 from maja_newsletter.mailer import Mailer
-from maja_newsletter.models import Newsletter
+from maja_newsletter.utils.excel import make_excel_content
+from maja_newsletter.utils.vcard import make_vcard_content
+from maja_newsletter.settings import EXPORT_FILE_NAME, EXPORT_EMAIL_SUBJECT
 
 
 @shared_task
@@ -11,3 +22,33 @@ def celery_send_newsletter(newsletter, *args, **kwargs):
     if mailer.can_send:
         mailer.run(send_all=True)
     return mailer.can_send
+
+
+@shared_task
+def export_excel(data, recipient, export_name=None, headers=None, force_csv=False, encoding='utf8'):
+    filedir = mkdtemp()
+    output_file = os.path.join(filedir, export_name)
+    with open(output_file, 'wb') as output:
+        output, mimetype, file_ext = make_excel_content(data, output, headers, force_csv, encoding)
+    final_path = '{0}.{1}'.format(output_file, file_ext)
+    shutil.copyfile(output_file, final_path)
+    testo = EXPORT_FILE_NAME
+    message = EmailMessage(EXPORT_EMAIL_SUBJECT, testo, settings.DEFAULT_FROM_EMAIL, [recipient])
+    message.attach_file(final_path, mimetype=mimetype)
+    message.send()
+    shutil.rmtree(filedir)
+
+
+@shared_task
+def export_vcard(data, recipient, export_name=None):
+    filedir = mkdtemp()
+    output_file = os.path.join(filedir, export_name)
+    with open(output_file, 'wb') as output:
+        output, mimetype, file_ext = make_vcard_content(data, output)
+    final_path = '{0}.{1}'.format(output_file, file_ext)
+    shutil.copyfile(output_file, final_path)
+    testo = EXPORT_FILE_NAME
+    message = EmailMessage(EXPORT_EMAIL_SUBJECT, testo, settings.DEFAULT_FROM_EMAIL, [recipient])
+    message.attach_file(final_path, mimetype=mimetype)
+    message.send()
+    shutil.rmtree(filedir)
--- maja_newsletter/utils/excel.py
+++ maja_newsletter/utils/excel.py
@@ -1,5 +1,4 @@
 """ExcelResponse for maja_newsletter"""
-# Based on http://www.djangosnippets.org/snippets/1151/
 import datetime
 
 import pytz
@@ -13,80 +12,86 @@ from django.utils import timezone
 class ExcelResponse(HttpResponse):
     """ExcelResponse feeded by queryset"""
 
-    def _make_naive(self, value):
-        if isinstance(value, basestring):
-            return value
-        if timezone.is_aware(value):
-            return timezone.make_naive(value, pytz.timezone(settings.TIME_ZONE))
-        return value
-
     def __init__(self, data, output_name='excel_data', headers=None,
                  force_csv=False, encoding='utf8'):
-        valid_data = False
-        if isinstance(data, ValuesQuerySet):
-            data = list(data)
-        elif isinstance(data, QuerySet):
-            data = list(data.values())
-        if hasattr(data, '__getitem__'):
-            if isinstance(data[0], dict):
-                if headers is None:
-                    headers = data[0].keys()
-                data = [[row[col] for col in headers] for row in data]
-                data.insert(0, headers)
-            if hasattr(data[0], '__getitem__'):
-                valid_data = True
-        assert valid_data is True, "ExcelResponse requires a sequence of sequences"
-
-        for item in data:
-            item[3] = self._make_naive(item[3])
-            item[10] = self._make_naive(item[10])
-
         import StringIO
         output = StringIO.StringIO()
-        # Excel has a limit on number of rows; if we have more than that, make a csv
-        use_xls = False
-        if len(data) <= 65536 and force_csv is not True:
-            try:
-                import xlwt
-            except ImportError:
-                pass
-            else:
-                use_xls = True
-        if use_xls:
-            book = xlwt.Workbook(encoding=encoding)
-            sheet = book.add_sheet('Sheet 1')
-            styles = {'datetime': xlwt.easyxf(num_format_str='yyyy-mm-dd hh:mm:ss'),
-                      'date': xlwt.easyxf(num_format_str='yyyy-mm-dd'),
-                      'time': xlwt.easyxf(num_format_str='hh:mm:ss'),
-                      'default': xlwt.Style.default_style}
-            for rowx, row in enumerate(data):
-                for colx, value in enumerate(row):
-                    if isinstance(value, datetime.datetime):
-                        cell_style = styles['datetime']
-                    elif isinstance(value, datetime.date):
-                        cell_style = styles['date']
-                    elif isinstance(value, datetime.time):
-                        cell_style = styles['time']
-                    else:
-                        cell_style = styles['default']
-                    sheet.write(rowx, colx, value, style=cell_style)
-            book.save(output)
-            mimetype = 'application/vnd.ms-excel'
-            file_ext = 'xls'
-        else:
-            for row in data:
-                out_row = []
-                for value in row:
-                    if not isinstance(value, basestring):
-                        value = unicode(value)
-                    value = value.encode(encoding)
-                    out_row.append(value.replace('"', '""'))
-                output.write('"%s"\n' %
-                             '","'.join(out_row))
-            mimetype = 'text/csv'
-            file_ext = 'csv'
-        output.seek(0)
+        output, mimetype, file_ext = make_excel_content(data, output, headers, force_csv, encoding)
         super(ExcelResponse, self).__init__(content=output.getvalue(),
                                             mimetype=mimetype)
-        self['Content-Disposition'] = 'attachment;filename="%s.%s"' % \
-            (output_name.replace('"', '\"'), file_ext)
+        self['Content-Disposition'] = 'attachment;filename="%s.%s"' % (
+            output_name.replace('"', '\"'), file_ext
+        )
+
+
+def _make_naive(value):
+    if isinstance(value, basestring):
+        return value
+    if timezone.is_aware(value):
+        return timezone.make_naive(value, pytz.timezone(settings.TIME_ZONE))
+    return value
+
+
+def make_excel_content(data, output=None, headers=None, force_csv=False, encoding='utf8'):
+    valid_data = False
+    if isinstance(data, ValuesQuerySet):
+        data = list(data)
+    elif isinstance(data, QuerySet):
+        data = list(data.values())
+    if hasattr(data, '__getitem__'):
+        if isinstance(data[0], dict):
+            if headers is None:
+                headers = data[0].keys()
+            data = [[row[col] for col in headers] for row in data]
+            data.insert(0, headers)
+        if hasattr(data[0], '__getitem__'):
+            valid_data = True
+    assert valid_data is True, 'ExcelResponse requires a sequence of sequences'
+
+    # Excel has a limit on number of rows; if we have more than that, make a csv
+    use_xls = False
+    if len(data) <= 65536 and force_csv is not True:
+        try:
+            import xlwt
+        except ImportError:
+            pass
+        else:
+            use_xls = True
+    if use_xls:
+        book = xlwt.Workbook(encoding=encoding)
+        sheet = book.add_sheet('Sheet 1')
+        styles = {'datetime': xlwt.easyxf(num_format_str='yyyy-mm-dd hh:mm:ss'),
+                  'date': xlwt.easyxf(num_format_str='yyyy-mm-dd'),
+                  'time': xlwt.easyxf(num_format_str='hh:mm:ss'),
+                  'default': xlwt.Style.default_style}
+        for rowx, row in enumerate(data):
+            for colx, value in enumerate(row):
+                if isinstance(value, datetime.datetime):
+                    cell_style = styles['datetime']
+                elif isinstance(value, datetime.date):
+                    cell_style = styles['date']
+                elif isinstance(value, datetime.time):
+                    cell_style = styles['time']
+                else:
+                    cell_style = styles['default']
+                if colx in (3, 10):
+                    value = _make_naive(value)
+                sheet.write(rowx, colx, value, style=cell_style)
+        book.save(output)
+        mimetype = 'application/vnd.ms-excel'
+        file_ext = 'xls'
+    else:
+        for rowx, row in enumerate(data):
+            out_row = []
+            for colx, value in enumerate(row):
+                if not isinstance(value, basestring):
+                    value = unicode(value)
+                if colx in (3, 10):
+                    value = _make_naive(value)
+                value = value.encode(encoding)
+                out_row.append(value.replace('"', '""'))
+            output.write('"%s"\n' % '","'.join(out_row))
+        mimetype = 'text/csv'
+        file_ext = 'csv'
+    output.seek(0)
+    return output, mimetype, file_ext
